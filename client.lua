8a02b1f2a0fa27e69fc2327b1137e9e1169a465f
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-28 22:37:11 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ RegisterNUICallback('drive', function(data)
   local lcoords = json.decode(data.garage_coords)
   local coords = vector3(lcoords.x, lcoords.y, lcoords.z)
   local SpotCoords = GetUnoccupiedSpot(coords)
-  TriggerServerEvent('lrp:GetVehicleFromGarage', GetPlayerServerId(GetPlayerIndex()), coords, data.plate, SpotCoords)
+  TriggerServerEvent('qb:GetVehicleFromGarage', GetPlayerServerId(GetPlayerIndex()), coords, data.plate, SpotCoords)
   UIDisplay = false
   SetNuiFocus(false, false)
   SendNUIMessage({
