2b7883906b459d4fff7474ed5e1135b838d52d72
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-28 22:36:54 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,3 +10,22 @@ RegisterNUICallback('drive', function(data)
     display = false
   })
 end)
+
+function GetUnoccupiedSpot(coords)
+  for _, v in pairs(Config.ParkingSpots) do
+    local SpotCoords = vector3(v.x, v.y, v.z)
+    local SpotDistance = Vdist(coords.x, coords.y, coords.z, SpotCoords.x, SpotCoords.y, SpotCoords.z)
+    if not IsVehicleParkedInSpot(SpotCoords) and SpotDistance < 45.0 then
+       return v
+    end
+  end
+end
+
+function IsVehicleParkedInSpot(ParkingSpot)
+  local ClosestVehicle = GetClosestVehicle(ParkingSpot.x, ParkingSpot.y, ParkingSpot.z, 2.0, 0, 71)
+  if ClosestVehicle ~= 0 then
+    return true
+  else
+    return false
+  end
+end
