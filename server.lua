0697213b708d707ecaa1bd958e0959353089c89c
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-28 22:36:44 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,18 +1,14 @@
-function GetUnoccupiedSpot(coords)
-  for _, v in pairs(Config.ParkingSpots) do
-    local SpotCoords = vector3(v.x, v.y, v.z)
-    local SpotDistance = Vdist(coords.x, coords.y, coords.z, SpotCoords.x, SpotCoords.y, SpotCoords.z)
-    if not IsVehicleParkedInSpot(SpotCoords) and SpotDistance < 45.0 then
-       return v
+RegisterServerEvent("lrp:GetVehicleFromGarage")
+AddEventHandler("lrp:GetVehicleFromGarage", function(player, coords, plate, spotcoords)
+  local LocationHash = GetHashKey(coords.x .. coords.y .. coords.z)
+  MySQL.Async.fetchAll('SELECT * FROM lrp_garages WHERE plate = @plate AND garage = @coords AND isout = 0 LIMIT 1', {
+    ['@coords'] = LocationHash,
+    ['@plate']  = plate
+  }, function(result)
+    if result[1] then
+      result[1]['coords'] = spotcoords
+      TriggerClientEvent('lrp:SpawnVehicle', player, result[1])
+      SetVehicleIsOut(plate)
     end
-  end
-end
-
-function IsVehicleParkedInSpot(ParkingSpot)
-  local ClosestVehicle = GetClosestVehicle(ParkingSpot.x, ParkingSpot.y, ParkingSpot.z, 2.0, 0, 71)
-  if ClosestVehicle ~= 0 then
-    return true
-  else
-    return false
-  end
-end
+  end) 
+end)
